51cd4a260bec5164ef1b3fa0abd49e7657632762
Stefan Dessens <stefan.dessens@gmail.com>
2014-04-15 15:15:13 +0200

    added visitor pattern class

---
+++ visitorpattern.py
@@ -0,0 +1,40 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Stefan
+#
+# Created:     15-04-2014
+# Copyright:   (c) Stefan 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+class VisitorPattern:
+	def __init__( self ):
+		self.__visitors__ = []
+
+	"""
+	Add a visitor to this instance.
+	An visitor is any object with an 'accept' method
+
+	@param object vis   the visitor to add
+	"""
+	def addVisitor( self, vis ):
+		self.__visitors__.append( vis );
+		assert( 'append' in dir(vis) )
+
+	"""
+	Select an visitor that accepts this object,
+	or None if no such visitor exists
+
+	@param object obj   the object that the visitor should accept
+	"""
+	def select( self, obj ):
+		for visitor in self.__visitors__:
+			if visitor.accept( obj ):
+				return visitor
+		return None
+
+
+if __name__ == '__main__':
+    main()
