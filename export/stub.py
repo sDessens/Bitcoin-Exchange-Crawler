ad3134257652e58bf50c3d8e44bceaaa59058b16
Stefan Dessens <stefan.dessens@gmail.com>
2014-04-18 13:08:51 +0200

    fixed a bug that caused sum visitor to return empty balancedata objects. Added stub export visitor

---
+++ export/stub.py
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Name          stub
+# Purpose:      Module is stub implementation of Export Visitor.
+#               Developers can use this file as an example when implementing
+#               additional exporters
+#
+# Author:       Stefan Dessens
+#
+# Created:      17-04-2014
+# Copyright:    (c) Stefan Dessens 2014
+# Licence:      TBD
+#-------------------------------------------------------------------------------
+
+## This function is required for every Visitor module
+def getInstance():
+    return StubVisitor()
+
+## this is a stub Export visitor. It provides an template that provides
+#  all required functionality for an Export visitor
+class StubVisitor:
+    def __init__(self):
+        pass
+
+    ## check if visitor accepts given object
+    #  @return true if object is accepted
+    def accept( self, json ):
+        try:
+            return json['type'] == 'stub'
+        except Exception as e:
+            return False
+
+    ## run the export algorithm.
+    #  this stub printing algorithm prints the identifier and final value of each
+    #  BalanceData to stdout.
+    #  @param data an {'identifier' -> BalanceData} map.
+    #  @param json contains implementation defined information about the export type
+    #  @return some sort of file array
+    def visit( self, json, data ):
+        print 'begin of stub export visitor output'
+        for k, v in data.items():
+            print '  {0:15s} = {1}'.format(k, v.interpolate( v.maxTimestampAsDateTime() ) )
+        print 'end of export visitor output'
+        return []
