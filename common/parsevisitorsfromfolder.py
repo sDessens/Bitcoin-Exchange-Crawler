8f5a2e34af816173b34250c3330ea730690f4198
Stefan Dessens <stefan.dessens@gmail.com>
2014-04-18 02:15:58 +0200

    added try .. catch for missing libraries

--- common/parsevisitorsfromfolder.py
+++ common/parsevisitorsfromfolder.py
@@ -12,19 +12,26 @@
 
 import visitorpattern
 
+##scan target folder for all modules that implement the
+# getInstance() function
+# @param moduleName name of the folder to query
+# @return visitorPattern object
 def getVisitorsFromFolder( moduleName ):
     visitors = visitorpattern.VisitorPattern()
 
     for item in __import__(moduleName).__all__:
-        module = __import__( moduleName + '.' + item )
-        attr = getattr( module, item )
         try:
-            visitors.addVisitor( attr.getInstance() )
-            print 'added visitor {0}/{1}'.format( moduleName, item )
-        except Exception as e:
-            if type(e) is AssertionError:
-                 print '{0}.py:'.format(item), str(e)
-            else:
-                print '{0}.py: unable to add exchange visitor.'.format( item )
+            module = __import__( moduleName + '.' + item )
+            attr = getattr( module, item )
+            try:
+                visitors.addVisitor( attr.getInstance() )
+                print 'added visitor {0}/{1}'.format( moduleName, item )
+            except Exception as e:
+                if type(e) is AssertionError:
+                     print '{0}.py:'.format(item), str(e)
+                else:
+                    print '{0}.py: unable to add exchange visitor.'.format( item )
+        except ImportError as e:
+            print 'unable to add visitor {0}/{1} because: {2}'.format( moduleName, item, str(e) )
 
     return visitors
