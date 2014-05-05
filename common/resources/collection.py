ce4212f128b30cc7275652e9dc0c21652979e08e
Stefan Dessens <stefan.dessens@gmail.com>
2014-05-05 19:19:58 +0200

    added select function to collection

--- common/resources/collection.py
+++ common/resources/collection.py
@@ -4,8 +4,34 @@ class Resource:
 
 class Collection( dict ):
     def __setitem__(self, key, value):
-        assert isinstance( value, Resource )
+        assert issubclass( value.__class__, Resource )
         super( Collection, self ).__setitem__( key, value )
+
+    ## select an subset of resources
+    #  @param selector. If plain string, select resource(s) with that identifier.
+    #  otherwise, interprent argument as array of strings, add resource in OR fashion.
+    #  @param mustBeType (optional) select only resources of that type.
+    #  @return an (key, resource) array of matched items. May return empty array if nothing matched.
+    def select(self, selector, mustBeType=Resource):
+        out = []
+
+        if isinstance( selector, str ):
+            for k, v in self.items():
+                if self._matches( k, selector ):
+                    out.append((k, v))
+        else:
+            for k, v in self.items():
+                for sel in selector:
+                    if self._matches( k, sel ):
+                        out.append((k, v))
+                        break
+
+        return out
+
+    def _matches(self, id, selector):
+        if id == selector:
+            return True
+        return False
 
     def report(self, msg = None):
         if msg is not None:
@@ -15,5 +41,3 @@ class Collection( dict ):
             for k, v in self.items():
                 if v.__class__ == id:
                     print '    {0:15s} => {1}'.format( k, str(v) )
-
-
