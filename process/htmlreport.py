3166a3fb6aed8ac5e67590c2ff1f05dbae5ffcc1
Jasper van Gelder <J_vangelder+git@live.nl>
2014-05-06 22:17:44 +0200

    added header formating based on period and periodcount, added documentation and added check in addvaluerow to prevent non BalanceData from being added

--- process/htmlreport.py
+++ process/htmlreport.py
@@ -1,17 +1,9 @@
-#-------------------------------------------------------------------------------
-# Name          HtmlReport
 # Purpose:      Module is implementation of an process visitor that exports data
-#               to a html report with tables.
-#
-# Author:       Jasper van Gelder
-#
-# Copyright:    (c) Jasper van Gelder 2014
-# Licence:      TBD
-#-------------------------------------------------------------------------------
 
-
-import common.balanceData
+from common.resources.fullBalance import FullBalance
+from common.balanceData import BalanceData
 from common.resources.file import File
+import common.tempFileLib
 
 from time import time
 from datetime import datetime
@@ -22,8 +14,8 @@ log = logging.getLogger( 'main.process.htmlreport' )
 def getInstance():
     return HtmlReportVisitor()
 
-## this is a stub Process visitor. It provides an template that provides
-#  all required functionality for an Process visitor
+## this is a Process visitor. It provides all required functionality 
+#  for an Process visitor
 class HtmlReportVisitor:
     def __init__(self):
         pass
@@ -37,12 +29,11 @@ class HtmlReportVisitor:
         except Exception as e:
             return False
 
-    ## run the process algorithm.
-    #  this stub printing algorithm prints the identifier and final value of each
-    #  BalanceData to stdout.
+    ## run the htmlreport process.
+    #  this visitor adds a html file to resources
+    #  
     #  @param json contains implementation defined information about the process type
-    #  @param resources contains array of common.writable.*
-    #  may return exception
+    #  @param resources contains array of common.resources.fullBalance.FullBalance
     def visit( self, json, resources ):
         tmpfile = common.tempFileLib.generateTempFile(True)
         if 'cssfile' in json:
@@ -59,9 +50,16 @@ class HtmlReportVisitor:
            
             #add the source to the tabledata
             log.info('adding sources to table')
-            for source in table['sources']:
-                tabledata.addValueRow(resources[source].value,source,table.get('type'))
+            balanceCollection = resources.selectMany(table['sources'],FullBalance)
+            #if no resources are found skip to the next table
+            if len(balanceCollection) <1:
+                log.warn('No resources found to add to table')
+                continue
+            for key, resource in balanceCollection:
+                tabledata.addValueRow(resource.value,key,table.get('type'))
+            
             tabledata.sort()
+            #add the total row and column if the options are set
             if table.get('totalrow'):
                 log.info('adding total row to table')
                 tabledata.addTotalRow()
@@ -89,23 +87,41 @@ class TableData:
         self._thetime = thetime
         #add headers to the tables
         self.__table.append(['Source'])
+        #determine the header format to be used
+        if period < (24*60*60):
+            headerformat = '%H:%M'
+            if periodcount > 24:
+                headerformat = '%a ' + headerformat
+        elif period < (7*24*60*60):
+            headerformat = '%a'
+            if periodcount > 7:
+                headerformat += ' %x'
+        else:
+            headerformat =  '%x'
         #add the headers based on date
         for i in reversed(xrange(self._periodcount)):
-            datestring = datetime.fromtimestamp(self._thetime - (i * self._period )).strftime('%a')
+            datestring = datetime.fromtimestamp(self._thetime - (i * self._period )).strftime(headerformat)
             self.__table[0].append(datestring)
-        
-    def addValueRow(self,balancedata,label,diff=None):
+    ## This function should be used to add rows to the table.
+    # @param balancedata requires balancedata
+    # @label the label for the row
+    # @valuetype string with the given type (currently only diff is implemented)
+    def addValueRow(self,balancedata,label,valuetype=None):
+        if not isinstance(balancedata,BalanceData):
+            log.warn('incorrect source type')
+            return
         valuerow=[]
         valuerow.append(label)
-        if diff == 'diff':
+        if valuetype == 'diff':
             for i in reversed(xrange(self._periodcount)):
                 valuerow.append("%6.3f"%float(balancedata.diff(self._thetime - ((i+1) * self._period),self._thetime - (i* self._period))))
         else:
             for i in reversed(xrange(self._periodcount)):
                 valuerow.append("%6.3f"%float(balancedata.interpolate(self._thetime - (i * self._period))))
         self.__table.append(valuerow)
-        print valuerow
-    
+
+    ## This function adds a total column.
+    # It sums up each row and adds the value to the end of the row
     def addDiffTotalColumn(self):
         #add the label for total column
         self.__table[0].append('Source sum')
@@ -113,7 +129,7 @@ class TableData:
         for row in range(1,len(self.__table)):
             total = sum([float(i) for i in self.__table[row][1:]])            
             self.__table[row].append(total)
-        
+    ## This function adds a totalRow below the last row    
     def addTotalRow(self):
         self.__table.append(['Period Total'])
         for column in range(1,len(self.__table[0])):
@@ -131,15 +147,14 @@ class TableData:
     def __getitem__(self,key):
         return self.__table[key]
         
-## Class generates the html
-#  This class contains functions to generate the html report
+## This class contains functions to generate the html report
 #  @param tmpFile Requires the fileHandle to write the report to.
 #  @param cssFilePath path to the css file that should be used
 #  @param title the title that should be used for the report.
 class HmtlReportGenerator:
     def __init__(self,tmpFile, cssFilePath,title):
         self.timeformat= '%d/%m/%Y %H:%M:%S'
-        self.Html_file = open(tmpFile,"w")
+        self.Html_file = open('myreport.html',"w")
         self.Html_file.write("""
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -150,7 +165,8 @@ class HmtlReportGenerator:
         <meta http-equiv="content-type" content="text/html;charset=utf-8" />
     </head>
     <body><div id="main">""")
-
+    ## This function adds any data you want in your html file
+    # @param htmldata the data you whish to insert
     def addCustomHTML(self,htmldata):
         self.Html_file.write(htmldata)
 
@@ -172,7 +188,6 @@ class HmtlReportGenerator:
         <tbody>""")
         #Add the values
         #skip the first key since we already asigned it
-
         tableiter = iter(tabledata)
         next(tableiter)
         for rowdata in tableiter:
@@ -194,7 +209,7 @@ class HmtlReportGenerator:
 
         self.Html_file.write("""
         </tbody></table></div>""")
-
+    ## This function closes the html tags and file
     def finalize(self):
         self.Html_file.write("""
         </div>
