85987246cb5bba32164edb1e96bb0b5fe48b3f57
Jasper van Gelder <j_vangelder\+git@live.nl>
2014-05-11 00:00:42 +0200

    renamed last week profit to last period and changed it so any period can be used

--- kpis/lastperiodprofit.py
+++ kpis/lastperiodprofit.py
@@ -1,4 +1,4 @@
-# This module implements last week profit kpi this can be added to the report. 
+# This module implements last period profit kpi this can be added to the report. 
 from common.resources.fullBalance import FullBalance
 from common.balanceData import BalanceData
 from common.resources.collection import Collection
@@ -6,16 +6,16 @@ from common.resources.collection import Collection
 from time import time
 from datetime import datetime
 import logging
-log = logging.getLogger( 'main.process.kpi.lastweekprofitKPIVisitor' )
+log = logging.getLogger( 'main.process.kpi.lastperiodprofitVisitor' )
 
 
 ## This function is required for every Visitor module
 def getInstance():
-    return lastweekprofitKPIVisitor()
+    return lastperiodprofitKPIVisitor()
 
 ## this is a Process visitor. It provides all required functionality 
 #  for an Process visitor
-class lastweekprofitKPIVisitor:
+class lastperiodprofitKPIVisitor:
     def __init__(self):
         pass
 
@@ -24,17 +24,20 @@ class lastweekprofitKPIVisitor:
     #  may not return exceptions
     def accept( self, json ):
         try:
-            return json['type'] == 'lastweekprofit'
+            return json['type'] == 'lastperiodprofit'
         except Exception as e:
             return False
-    
+    ##returns the profit from a certain period till now
+    # @param json a json dict containing data: a list of resource strings,
+    # period: the amount of seconds to the last period form now. A day = 24*60*60,
+    # @param resources a resource object
     def visit( self, json, resources ):
         balanceCollection = resources.selectMany(json['data'],FullBalance)
         thetime = time()
-        week = 7*24*60*60
+        period = json['period']
         profit = 0.0
         for key,balance in balanceCollection:
-            profit += balance.value.diff(thetime - week,thetime )
+            profit += balance.value.diff(thetime - period,thetime)
         kpivalue = profit
         indicator = ""
         return (indicator,kpivalue)
